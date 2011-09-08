4e8c8c2133d42986fd75e27d12b45ac2a2f49405
Ingy dot Net <ingy@ingy.net>
2011-09-08 15:24:48 +0200

    Released version 0.10

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -1,3 +1,10 @@
+##
+# name:      Mo
+# abstract:  Micro Objects
+# author:    Ingy döt Net <ingy@cpan.org>
+# license:   perl
+# copyright: 2011
+
 use 5.010;
 package Mo;
 use strict;
@@ -44,3 +51,15 @@ sub extends {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+    package Reaper;
+    use Mo 'Cow Bell';
+    extends 'SNL';
+
+    has noise => (is =>'ro');
+
+=head1 DESCRIPTION
+
+Moose led to Mouse led to Moo led to Mo. M, anyone?
