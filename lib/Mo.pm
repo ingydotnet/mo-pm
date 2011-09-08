bd964cc6e17eb08ac7211feda94abfd871817e3b
Philippe Bruhat (BooK) <book@cpan.org>
2011-09-08 17:53:06 +0200

    remove some warnings

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -6,10 +6,10 @@ sub import {
     my $p = caller;
     my %m = (
         new     => sub { bless { @_[ 1 .. $#_ ] }, $_[0] },
-        extends => sub { @{ caller . '::ISA' } = $_[0] },
+        extends => sub { @{ (caller) . '::ISA' } = $_[0] },
         has     => sub {
             my $n = $_[0];
-            *{ caller . "::$n" } = sub {
+            *{ (caller) . "::$n" } = sub {
                 @_ - 1 ? $_[0]->{$n} = $_[1] : $_[0]->{$n};
             };
         },
