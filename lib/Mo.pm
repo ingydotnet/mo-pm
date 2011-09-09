da3098de6c3a161faeb020b0f92115c280ab82f9
vti <viacheslav.t@gmail.com>
2011-09-09 20:19:27 +0300

    Made it shorter

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -3,14 +3,13 @@ require strict; require warnings;
 our $VERSION = '0.12';
 sub import {
     strict->import; warnings->import;
-    my $p = caller;
-    @{$p.'::ISA'} = $_[0];
-    *{$p.'::extends'} = sub { @{(caller).'::ISA'} = $_[0] };
-    *{$p.'::has'} = sub {
+    @{$_.'::ISA'} = $_[0],
+    *{$_.'::extends'} = sub { @{(caller).'::ISA'} = $_[0] },
+    *{$_.'::has'} = sub {
         my ($n, %a) = @_;
         *{(caller)."::$n"} =
           sub {@_-1?$_[0]{$n}=$_[1]:$_[0]{$n}//=($a{default}//sub{})->($_[0])}
-    };
+    } for scalar caller;
 }
 sub new {
     my $s = bless { @_[1..$#_] }, $_[0]; $s->can('BUILD') && $s->BUILD; $s
