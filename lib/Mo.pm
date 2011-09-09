13098735b5d658bf8d962d90d0d8cd013d083850
Ingy dot Net <ingy@ingy.net>
2011-09-09 07:52:38 +0200

    Released version 0.12

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -1,17 +1,16 @@
 package Mo;
-
-our $VERSION = '0.11';
-
+require strict; require warnings;
+our $VERSION = '0.12';
 sub import {
+    strict->import; warnings->import;
     my $p = caller;
-    *{$p.'::extends'} = sub { @{caller.'::ISA'} = $_[0] };
+    @{$p.'::ISA'} = $_[0];
+    *{$p.'::extends'} = sub { @{(caller).'::ISA'} = $_[0] };
     *{$p.'::has'} = sub {
         my $n = $_[0];
-        *{caller."::$n"} = sub { @_-1 ? $_[0]->{$n} = $_[1] : $_[0]->{$n} };
+        *{(caller)."::$n"} = sub { @_-1 ? $_[0]->{$n} = $_[1] : $_[0]->{$n} };
     };
-    push @{$p.'::ISA'}, $_[0];
 }
-
-sub new { my $s = bless { @_[1..$#_] }, $_[0]; $s->can('BUILD') && $s->BUILD; $s }
-
-1;
+sub new {
+    my $s = bless { @_[1..$#_] }, $_[0]; $s->can('BUILD') && $s->BUILD; $s
+}
