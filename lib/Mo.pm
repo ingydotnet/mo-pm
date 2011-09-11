555c525df0c800f287f707caec996f04d1a7155a
Ingy dot Net <ingy@ingy.net>
2011-09-11 11:27:34 +0200

    Released version 0.19

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -1,13 +1,2 @@
-package Mo; require strict; require warnings; $Mo::VERSION = '0.18';
-sub import {strict->import;warnings->import;my $p=caller;@{$p.'::ISA'}=$_[0];
-  *{$p.'::extends'} = sub {@{(caller).'::ISA'}=@_;eval "require $_" for @_};
-  *{$p.'::has'} = sub { my ($n, %a) = @_; my($d,$b)=@a{qw(default builder)};
-  *{(caller)."::$n"} = $d ? sub { $#_ ? ($_[0]{$n} = $_[1]) : (exists $_[0]{$n})
-      ? $_[0]{$n} : ($_[0]{$n} = $d->($_[0])) } :
-    $b ? sub { $#_ ? ($_[0]{$n} = $_[1]) : (exists $_[0]{$n})
-      ? $_[0]{$n} : ($_[0]{$n} = $_[0]->$b) }
-  : sub { $#_ ?  $_[0]{$n} = $_[1] : $_[0]{$n} } }; }
-my $i;$i = sub {my ($a, $c) = @_;for my $p (@{$c."::ISA"}) { next if $p eq 'Mo';
-    unshift @$a, $p; $i->($a, $p); } @$a; };
-sub new { my $c=shift; my$s=bless{@_},$c;
-    do{&{$_."::BUILD"}($s) if defined&{$_."::BUILD"}} for ($i->([$c],$c)); $s }
+package Mo;require strict;require warnings;$Mo::VERSION='0.19';
+sub import{strict->import;warnings->import;my$p=caller;@{$p.'::ISA'}=$_[0];*{$p.'::extends'}=sub{@{(caller).'::ISA'}=@_;eval"require($_)"for@_;};*{$p.'::has'}=sub{my($n,%a)=@_;my($d,$b)=@a{qw(default builder)};*{(caller)."::$n"}=$d?sub{$#_?($_[0]{$n}=$_[1]):(exists$_[0]{$n})?$_[0]{$n}:($_[0]{$n}=$d->($_[0]));}:$b?sub{$#_?($_[0]{$n}=$_[1]):(exists$_[0]{$n})?$_[0]{$n}:($_[0]{$n}=$_[0]->$b);}:sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n};}};}my$i;$i=sub{my($a,$c)=@_;for my$p(@{$c."::ISA"}){next if$p eq'Mo';unshift@$a,$p;$i->($a,$p);}@$a;};sub new{my$c=shift;my$s=bless{@_},$c;do{&{$_."::BUILD"}($s)if defined&{$_."::BUILD"};}for($i->([$c],$c));$s;}
