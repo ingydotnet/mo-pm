4ad95df1fbc38feab20c50d1c0e27b01dd153e2e
Matt S Trout <mst@shadowcat.co.uk>
2011-09-12 17:37:00 +0000

    excise extraneous parens

--- lib/Mo.pm
+++ lib/Mo.pm
@@ -1,3 +1,3 @@
 package Mo;
 $VERSION='0.22';
-no warnings;sub import{import warnings;$^H|=0x602;my$p=caller."'";@{$p.ISA}=Mo::_;*{$p.extends}=sub{@{$p.ISA}=$_[0];eval"no $_[0]()"};*{$p.has}=sub{my($n,%a)=@_;my$d=$a{default}||$a{builder};*{$p.$n}=$d?sub{$#_?($_[0]{$n}=$_[1]):exists$_[0]{$n}?$_[0]{$n}:($_[0]{$n}=$_[0]->$d)}:sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}}}}sub Mo'_'new{my$c=shift;my$s=bless{@_},$c;my@c;do{unshift@c,$c."'BUILD"}while$c=${$c."'ISA"}[0];defined&$_&&&$_($s)for@c;$s}
+no warnings;sub import{import warnings;$^H|=0x602;my$p=caller."'";@{$p.ISA}=Mo::_;*{$p.extends}=sub{@{$p.ISA}=$_[0];eval"no $_[0]()"};*{$p.has}=sub{my($n,%a)=@_;my$d=$a{default}||$a{builder};*{$p.$n}=$d?sub{$#_?$_[0]{$n}=$_[1]:exists$_[0]{$n}?$_[0]{$n}:($_[0]{$n}=$_[0]->$d)}:sub{$#_?$_[0]{$n}=$_[1]:$_[0]{$n}}}}sub Mo'_'new{my$c=shift;my$s=bless{@_},$c;my@c;do{unshift@c,$c."'BUILD"}while$c=${$c."'ISA"}[0];defined&$_&&&$_($s)for@c;$s}
