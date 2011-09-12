6c2321fc89b30d0e599d51c675762cf84da2561a
Matt S Trout <mst@shadowcat.co.uk>
2011-09-12 13:03:56 +0000

    small amounts of extra golf

--- src/Mo.pm
+++ src/Mo.pm
@@ -2,14 +2,14 @@ package Mo; $Mo::VERSION = '0.21';
 
 sub import {
     require "$_.pm", $_->import for 'strict', 'warnings';
-    my $p = caller;
-    @{ $p . '::ISA' } = 'Mo::O';
-    *{ $p . '::extends' } =
-      sub { @{ (caller) . '::ISA' } = $_[0]; eval "require($_[0])" };
-    *{ $p . '::has' } = sub {
+    my $p = caller().'::';
+    @{ $p . 'ISA' } = 'Mo::O';
+    *{ $p . 'extends' } =
+      sub { @{ $p . 'ISA' } = $_[0]; eval "require($_[0])" };
+    *{ $p . 'has' } = sub {
         my ( $n, %a ) = @_;
         my $d = $a{default}||$a{builder};
-        *{ (caller) . "::$n" } = $d
+        *{ $p . "$n" } = $d
           ? sub {
             $#_ ? ( $_[0]{$n} = $_[1] )
               : ( exists $_[0]{$n} ) ? $_[0]{$n}
