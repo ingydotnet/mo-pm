358717f1a1ebb4b1a41e525b1217a0ccfbbb0124
Ingy dot Net <ingy@ingy.net>
2011-09-26 00:18:25 +0200

    figured out how to grab has function.

--- lib/Mo/default.pm
+++ lib/Mo/default.pm
@@ -1,29 +1,25 @@
 package Mo::default;
+# use strict;
 
+$YAML::DumpCode = 1;
 sub e {
     my $P = shift;
     my %s = @_;
-    %{{
-        @_,
-        has => sub {
-            my ( $n, %a ) = @_;
-            my $d = $a{default}||$a{builder};
-            *{ $P . "::$n" } = $d
-                ? sub {
-                    $#_
-                      ? $_[0]{$n} = $_[1]
-                        : ! exists $_[0]{$n}
-                          ? $_[0]{$n} = $_[0]->$d
-                          : $_[0]{$n}
-                }
-# XXX This code is duplicated from Mo. Should be wrapped somehow.
-                : sub {
-                    $#_
-                        ? $_[0]{$n} = $_[1]
-                        : $_[0]{$n};
-                };
-        },
-    }};
+    my $h = $s{has};
+    $s{has} = sub {
+        my ( $n, %a ) = @_;
+        my $d = $a{default}||$a{builder};
+        *{ $P . "::$n" } = $d
+            ? sub {
+                $#_
+                  ? $_[0]{$n} = $_[1]
+                    : ! exists $_[0]{$n}
+                      ? $_[0]{$n} = $_[0]->$d
+                      : $_[0]{$n}
+            }
+            : $h->($n);
+    };
+    %s;
 }
 
 1;
