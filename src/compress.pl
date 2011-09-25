515750b63d59dc09091a53cb318f00971dc82b44
Christian Walde <walde.christian@googlemail.com>
2011-09-25 14:16:53 +0200

    split golfing script into subs

--- src/compress.pl
+++ src/compress.pl
@@ -9,17 +9,29 @@ my $text = do {
     <>;
 };
 
-my $tree = PPI::Document->new( \$text );
-
-$text =~ s/^#.*\n//mg;
-#$text =~ s/\s*#'.*//;
-#$text =~ s/(\w)\s+([^\w])/$1$2/g;
-#$text =~ s/([^\w])\s+/$1/g;
-#$text =~ s/;\}/}/g;
-#$text =~ s/(\$VERSION.*?;)/\n$1\n/;
-#$text =~ s/(no\$)/no \$/;
-#$text .= "\n";
-
 binmode STDOUT;
-print $text;
-#print $tree->serialize;
+print golf_with_regex( $text );
+#print golf_with_ppi( $text );
+
+sub golf_with_regex {
+    my ( $text ) = @_;
+
+    $text =~ s/^#.*\n//mg;
+    #$text =~ s/\s*#'.*//;
+    #$text =~ s/(\w)\s+([^\w])/$1$2/g;
+    #$text =~ s/([^\w])\s+/$1/g;
+    #$text =~ s/;\}/}/g;
+    #$text =~ s/(\$VERSION.*?;)/\n$1\n/;
+    #$text =~ s/(no\$)/no \$/;
+    #$text .= "\n";
+
+    return $text;
+}
+
+sub golf_with_ppi {
+    my ( $text ) = @_;
+
+    my $tree = PPI::Document->new( \$text );
+
+    return $tree->serialize;
+}
