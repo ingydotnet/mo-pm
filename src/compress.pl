10bc0cd9890090d449b1b496af2412db7d06437d
Christian Walde <walde.christian@googlemail.com>
2011-09-25 14:25:58 +0200

    enabled stripping of white-space after word and before non-word chars

--- src/compress.pl
+++ src/compress.pl
@@ -20,8 +20,8 @@ sub golf_with_regex {
 
     $text =~ s/^#.*\n//mg;
     $text =~ s/\s*#'.*//;
+    $text =~ s/(\w)\s+([^\w])/$1$2/g;
 
-    #$text =~ s/(\w)\s+([^\w])/$1$2/g;
     #$text =~ s/([^\w])\s+/$1/g;
     #$text =~ s/;\}/}/g;
     #$text =~ s/(\$VERSION.*?;)/\n$1\n/;
