67b0c0226867221da7a660122fac9cfca1fa7eb7
Christian Walde <walde.christian@googlemail.com>
2011-09-25 14:13:19 +0200

    disable golfing

--- src/compress.pl
+++ src/compress.pl
@@ -5,14 +5,14 @@ my $text = do {
     <>;
 };
 
-$text =~ s/^#.*\n//mg;
-$text =~ s/\s*#'.*//;
-$text =~ s/(\w)\s+([^\w])/$1$2/g;
-$text =~ s/([^\w])\s+/$1/g;
-$text =~ s/;\}/}/g;
-$text =~ s/(\$VERSION.*?;)/\n$1\n/;
-$text =~ s/(no\$)/no \$/;
-$text .= "\n";
+#$text =~ s/^#.*\n//mg;
+#$text =~ s/\s*#'.*//;
+#$text =~ s/(\w)\s+([^\w])/$1$2/g;
+#$text =~ s/([^\w])\s+/$1/g;
+#$text =~ s/;\}/}/g;
+#$text =~ s/(\$VERSION.*?;)/\n$1\n/;
+#$text =~ s/(no\$)/no \$/;
+#$text .= "\n";
 
 binmode STDOUT;
 print $text;
