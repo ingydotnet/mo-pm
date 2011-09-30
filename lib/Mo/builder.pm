10b7ffeeb1d78fcda6d4c698ef4cfd79e1770f56
Alessandro Ranellucci <aar@cpan.org>
2011-09-30 16:39:27 +0200

    Rebuilt minified modules

--- lib/Mo/builder.pm
+++ lib/Mo/builder.pm
@@ -1,3 +1,3 @@
-package Mo::builder;$K="Mo::";
-$VERSION=0.24;
+package Mo::builder;my$K="Mo::";
+$VERSION=0.25;
 *{$K.'builder::e'}=sub{my($P,$p,$e)=@_;$p->{builder}=sub{my($m,$n,%a)=@_;my$b=$a{builder}or return$m;sub{$#_?$m->(@_):!exists$_[0]{$n}?$_[0]{$n}=$_[0]->$b:$m->(@_)}}};
--- lib/Mo/class_xsaccessor.pm
+++ lib/Mo/class_xsaccessor.pm
@@ -1,3 +1,3 @@
-package Mo::class_xsaccessor;
-$VERSION=0.24;
-package Mo;$K=__PACKAGE__."::";require Class::XSAccessor;*{$K.'class_xsaccessor::e'}=sub{my($P,$p,$e,$f)=@_;$P=~s/::$//;$e->{has}=sub{my($n,%a)=@_;Class::XSAccessor->import(class=>$P,accessors=>{$n=>$n})}unless grep!/class_xsaccessor/,@$f};
+package Mo::class_xsaccessor;my$K="Mo::";
+$VERSION=0.25;
+require Class::XSAccessor;*{$K.'class_xsaccessor::e'}=sub{my($P,$p,$e,$f)=@_;$P=~s/::$//;$e->{has}=sub{my($n,%a)=@_;Class::XSAccessor->import(class=>$P,accessors=>{$n=>$n})}unless grep!/class_xsaccessor/,@$f};
