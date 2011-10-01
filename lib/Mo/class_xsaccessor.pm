df38160d950272f78ddbcc70aeab520a0a676e33
Ingy dot Net <ingy@ingy.net>
2011-10-01 11:28:08 +0100

    Remove class_xsaccessor from master for now.

--- lib/Mo/class_xsaccessor.pm
+++
@@ -1,3 +0,0 @@
-package Mo::class_xsaccessor;my$K="Mo::";
-$VERSION=0.25;
-require Class::XSAccessor;*{$K.'class_xsaccessor::e'}=sub{my($P,$e,$h,$features)=@_;$P=~s/::$//;$e->{has}=sub{my($n,%a)=@_;Class::XSAccessor->import(class=>$P,accessors=>{$n=>$n})}unless grep!/class_xsaccessor/,@$features};
