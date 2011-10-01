079230e2ed9fd695d422a374357c39eb557412d1
Ingy dot Net <ingy@ingy.net>
2011-10-01 11:21:06 +0100

    Start adding comments to Mo.pm

--- src/Mo.pm
+++ src/Mo.pm
@@ -1,20 +1,36 @@
+# The first two lines are left alone in the compressed source.
 package Mo;
 $VERSION = 0.25;
 
+# 'no' is a shorter 'use', but we really don't want warnings.
 no warnings;
+# We need the package name to be relocatable for inlining. Adding the :: makes
+# everything shorter thereafter. :: is a bareword string. We do that a lot for
+# golfing reasons. Scary.
 my $MoPKG = __PACKAGE__.::;
 
+# This is our minimal constructor. Can we make it faster? We should have tests
+# for that.
 *{$MoPKG.Object::new} = sub {
     bless {@_[1..$#_]}, $_[0];
 };
 
 *{$MoPKG.import} = sub {
+    # Set warnings for the caller.
     import warnings;
+    # This is a golf for: use strict(); strict->import;
     $^H |= 1538;
     my $caller_pkg = caller.::;
     shift;
     my (%exports, %handlers);
-    eval "no Mo::$_", &{$MoPKG.$_.::e}($caller_pkg, \%exports, \%handlers, \@_) for @_;
+    # Load each feature and call its &e.
+    eval "no Mo::$_",
+        &{$MoPKG.$_.::e}(
+            $caller_pkg,
+            \%exports,
+            \%handlers,
+            \@_
+        ) for @_;
     %exports = (
         extends, sub {
             eval "no $_[0]()";
@@ -28,11 +44,13 @@ my $MoPKG = __PACKAGE__.::;
                         ? $_[0]{$name} = $_[1]
                         : $_[0]{$name};
                 };
-            $method = $handlers{$_}->($method, $name, @_) for sort keys %handlers;
+            $method = $handlers{$_}->($method, $name, @_)
+                for sort keys %handlers;
             *{ $caller_pkg . $name } = $method;
         },
         %exports,
     );
-    *{ $caller_pkg . $_} = $exports{$_} for keys %exports;
+    *{ $caller_pkg . $_} = $exports{$_}
+        for keys %exports;
     @{ $caller_pkg . ISA } = $MoPKG.Object;
 };
