0ba10edf275300b6773b0c5dd9b977b41dd555c1
Ingy dot Net <ingy@ingy.net>
2011-10-03 18:03:48 +0100

    Released version 0.26

--- src/Mo/exporter.pm
+++ src/Mo/exporter.pm
@@ -3,8 +3,7 @@ my $MoPKG = "Mo::";
 $VERSION = 0.26;
 
 *{$MoPKG.'exporter::e'} = sub {
-    my ($caller_pkg, $exports) = @_;
-    @{$MoPKG.EXPORT};
+    my ($caller_pkg) = @_;
     if (defined @{$MoPKG.EXPORT}) {
         *{$caller_pkg.$_} = \&{$MoPKG.$_}
             for @{$MoPKG.EXPORT};
