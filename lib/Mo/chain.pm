1ec924519d034f30bf411e3f29c5348a399715a7
forwardever <git.forwardever@googlemail.com>
2011-10-05 23:48:56 +0200

    cleanup

--- lib/Mo/chain.pm
+++ lib/Mo/chain.pm
@@ -1,14 +1,3 @@
-package Mo::chain;
-my $MoPKG = "Mo::";
-$VERSION = 0.26;
-
-*{$MoPKG.'chain::e'} = sub {
-    my ($caller_pkg, $exports, $options) = @_;
-    $options->{chain} = sub {
-        my ($method, $name, %args) = @_;
-        $args{chain} or return $method;
-        sub {
-            $#_ ? ($method->(@_) and return $_[0]) : $method->(@_);
-        };
-    };
-};
+package Mo::chain;my$M="Mo::";
+$VERSION=0.26;
+*{$M.'chain::e'}=sub{my($P,$e,$o)=@_;$o->{chain}=sub{my($m,$n,%a)=@_;$a{chain}or return$m;sub{$#_?($m->(@_)and return$_[0]):$m->(@_)}}};
---
+++ src/Mo/chain.pm
@@ -0,0 +1,14 @@
+package Mo::chain;
+my $MoPKG = "Mo::";
+$VERSION = 0.26;
+
+*{$MoPKG.'chain::e'} = sub {
+    my ($caller_pkg, $exports, $options) = @_;
+    $options->{chain} = sub {
+        my ($method, $name, %args) = @_;
+        $args{chain} or return $method;
+        sub {
+            $#_ ? ($method->(@_) and return $_[0]) : $method->(@_);
+        };
+    };
+};
