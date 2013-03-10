bbe8ecef45fb28720eb2573b61ceb8f48c960f59
Carlos Lima <carlos@cpan.org>
2013-03-10 17:59:02 +0800

    Removes nonlazy.pm

--- lib/Mo/nonlazy.pm
+++
@@ -1,3 +0,0 @@
-package Mo::nonlazy;my$M="Mo::";
-$VERSION=0.33;
-*{$M.'nonlazy::e'}=sub{${shift.N}=1};
--- src/Mo/nonlazy.pm
+++
@@ -1,7 +0,0 @@
-package Mo::nonlazy;
-my $MoPKG = "Mo::";
-$VERSION = 0.33;
-
-*{ $MoPKG . 'nonlazy::e' } = sub {
-    ${ shift . NONLAZY } = 1;
-};
