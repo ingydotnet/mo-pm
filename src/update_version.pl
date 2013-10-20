7394be7c6fdff58fd3322e2c82895a682945ce28
Carlos Lima <carlos@cpan.org>
2013-10-21 00:04:46 +0800

    Add script and Makefile target to update version

---
+++ src/update_version.pl
@@ -0,0 +1,29 @@
+#!perl
+use strict;
+use warnings;
+use 5.010;
+use File::Basename;
+use File::Find;
+use File::Slurp;
+
+my $basedir = dirname(__FILE__);
+
+my ($version) = read_file("$basedir/Mo.pm") =~ /\$VERSION[\s=']+([\d.]+)/;
+die 'Could not read version from Mo.pm' unless $version;
+say "Setting version to: $version";
+
+my @files;
+find(
+  sub {
+    push @files, $File::Find::name if $File::Find::name =~ /\.pm\z/;
+  },
+  $basedir
+);
+
+foreach my $f (@files) {
+    my $code = read_file($f);
+    if ($code !~ /\$VERSION=$version;/) {
+        $code =~ s/\$VERSION[\s=']+[\d.]+;/\$VERSION=$version;/;
+        write_file($f, $code);
+    }
+}
